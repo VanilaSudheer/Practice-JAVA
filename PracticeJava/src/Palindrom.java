3a432b9faa483b61e756ee51fb47d71e37b21f47
sudhe <vanisudheer@gmail.com>
2023-09-28 19:39:17 -0400

    Palindrom

--- PracticeJava/src/Palindrom.java
+++ PracticeJava/src/Palindrom.java
@@ -1,5 +1,5 @@
 public class Palindrom {
-    String s = "abcdedcxyba";
+    String s = "abcdeedcbaxx";
 
     public int isPalindrom(String str){
         int n =str.length();
@@ -14,11 +14,13 @@ public class Palindrom {
         int l = s.length();
         if(idx == -1) System.out.println("Palindrom");
         else{
+            System.out.println(idx+" try change this  "+ s.charAt(idx));
             String temp1 = s.substring(0,idx) + s.substring(idx+1);
-            if(isPalindrom(temp1) == -1) System.out.println(idx);
+            if(isPalindrom(temp1) == -1) System.out.println("success "+ idx);
             else {
+                System.out.println(l-idx-1+" try change this  "+ s.charAt(l-idx-1));
                 String temp2 = s.substring(0,l-1-idx) + s.substring(l-idx);
-                if(isPalindrom(temp2) == -1) System.out.println(l-idx-1);
+                if(isPalindrom(temp2) == -1) System.out.println("success "+ (l-idx-1));
                 else System.out.println("Not Palindrom");
             }
 

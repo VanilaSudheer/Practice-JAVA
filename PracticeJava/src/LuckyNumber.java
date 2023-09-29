4f9155aab030cf9a5a290ce84b5ad678809e69a8
sudhe <vanisudheer@gmail.com>
2023-09-29 18:50:10 -0400

    LuckyNumber

--- PracticeJava/src/LuckyNumber.java
+++ PracticeJava/src/LuckyNumber.java
@@ -1,18 +1,13 @@
 public class LuckyNumber {
     String n = "148";
-    public String sumOfDigits(String n){
-        int d,sum = 0;
-        if(n.length() == 1) return n;
-        for(char ch : n.toCharArray()){
-            d =Character.getNumericValue(ch);
-            sum += d;
-        }
-        n = Integer.toString(sum);
-        return sumOfDigits(n);
-    }
+
     public void result(){
-        String superDigit = sumOfDigits(n);
-        System.out.println(superDigit);
+        int num = 0;
+        for(char ch : n.toCharArray()){
+            num += Character.getNumericValue(ch);
+            num %= 9;
+        }
+        System.out.println("Lucky Number " + num);
 
 
     }

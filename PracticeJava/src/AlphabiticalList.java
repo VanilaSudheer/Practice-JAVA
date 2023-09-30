ae7166d01388fbd473faa52e5016a480cd0a2159
sudhe <vanisudheer@gmail.com>
2023-09-30 15:32:36 -0400

    alphabeticalList

--- PracticeJava/src/AlphabiticalList.java
+++ PracticeJava/src/AlphabiticalList.java
@@ -4,19 +4,38 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class AlphabiticalList {
-    List<String> grid1 = Arrays.asList("zbacd","fghij","olmkn","trpqs","xywuv");
-    List<String> grid = Arrays.asList("abc","ade","efg");
+    List<String> grid1 = Arrays.asList("zbacd", "fghij", "olmkn", "trpqs", "xywuv");
+    List<String> grid = Arrays.asList("abc", "ade", "efg");
     List<String> sortedGrid = grid.stream()
             .sorted()
             .collect(Collectors.toList());
-    boolean isAlphabetical = IntStream.range(0, grid.get(0).length())
-            .allMatch(col -> IntStream.range(1, grid.size())
-                    .allMatch(row -> sortedGrid.get(row).charAt(col) >= sortedGrid.get(row - 1).charAt(col)));
+    /***
+
+     boolean isAlphabetical = IntStream.range(0, grid.get(0).length())
+     .allMatch(col -> IntStream.range(1, grid.size())
+     .allMatch(row -> sortedGrid.get(row).charAt(col) >= sortedGrid.get(row - 1).charAt(col)));
+     **/
 
     public void result(){
         System.out.println(grid);
         System.out.println(sortedGrid);
-        System.out.println(isAlphabetical);
+        int m = sortedGrid.size();
+        int n = sortedGrid.get(0).length();
+        boolean toContinue = true;
+        int i = 0;
+        while (toContinue && i<n){
+            for (int j = 1; j<m;j++){
+                if(sortedGrid.get(j).charAt(i)<sortedGrid.get(j-1).charAt(i)){
+                    System.out.println("not AlphabeticalColumn");
+                    toContinue = false;
+                    break;
+                }
+            }
+            i++;
+
+        }
+        if(toContinue) System.out.println("alphabetical");
+
     }
 
 
